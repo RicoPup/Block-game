dad0aaf9de85e641e6598d41216483588bcd1c6f
Rico <Rico-dawg@hotmail.co.uk>
2020-05-06 14:14:26 +0100

    Object oriented random sizes

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ import sys
 
 pygame.init()
 
+pygame.mixer_music.load("Techno.wav")
+pygame.mixer_music.play(-1)
+
 WIDTH = 800
 HEIGHT = 600
 
@@ -12,12 +15,54 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 BACKGROUND_COLOUR = (0, 0, 0)
 
-player_size = 50
-player_pos = [WIDTH/2, HEIGHT-2*player_size]
 
-enemy_size = 50
-enemy_pos = [random.randint(0, WIDTH - enemy_size), 0]
-enemy_list = [enemy_pos]
+class Player:
+    size = 50
+    pos = [WIDTH/2, HEIGHT-2*size]
+
+    def __init__(self, size, pos):
+        self.size = size
+        self.pos = pos
+
+    def get_size(self):
+        return self.size
+
+    def get_pos(self):
+        return self.pos
+
+
+p1 = Player(40, [int(WIDTH/2), int(HEIGHT-100)])
+
+
+class Enemy:
+    size = 50
+    pos = [WIDTH/2, HEIGHT-2*size]
+
+    def __init__(self, size, pos):
+        self.size = size
+        self.pos = pos
+
+    def get_size(self):
+        return self.size
+
+    def get_pos(self):
+        return self.pos
+
+
+enemy_list = []
+
+for i in range(1):
+    e_size = random.randint(20, 100)
+    e = Enemy(e_size, [random.randint(0, WIDTH - e_size), int(0+(2*e_size))])
+    enemy_list.append(e)
+
+
+# player_size = 50
+# player_pos = [WIDTH/2, HEIGHT-2*player_size]
+
+# enemy_size = 50
+# enemy_pos = [random.randint(0, WIDTH - enemy_size), 0]
+# enemy_list = [enemy_pos]
 
 SPEED = 10
 
@@ -25,68 +70,69 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game_over = False
 
-score = 0
+_score = 0
 
 clock = pygame.time.Clock()
 
 myFont = pygame.font.SysFont("monospace", 35)
 
 
-def set_level(score, SPEED):
+def set_level(score, spd):
     if score < 10:
-        SPEED = 5
+        spd = 5
     elif score < 20:
-        SPEED = 8
+        spd = 8
     elif score < 30:
-        SPEED = 10
+        spd = 10
     elif score < 40:
-        SPEED = 13
+        spd = 13
     elif score < 60:
-        SPEED = 15
+        spd = 15
     elif score < 80:
-        SPEED = 20
-    return SPEED
+        spd = 20
+    return spd
 
 
-def drop_enemies(enemy_list):
+def drop_enemies(enemies):
     delay = random.random()
-    if len(enemy_list) < 10 and delay < 0.05:
-        x_pos = random.randint(0, WIDTH - enemy_size)
-        y_pos = 0
-        enemy_list.append([x_pos, y_pos])
+    if len(enemies) < 10 and delay < 0.1:
+        for i in range(1):
+            e_size = random.randint(20, 100)
+            e = Enemy(e_size, [random.randint(0, WIDTH - e_size), int(0 + 10)])
+            enemies.append(e)
 
 
-def draw_enemies(enemy_list):
-    for enemy_pos in enemy_list:
-        pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+def draw_enemies(enemies):
+    for enemy in enemies:
+        pygame.draw.rect(screen, BLUE, (enemy.get_pos()[0], enemy.get_pos()[1], enemy.get_size(), enemy.get_size()))
 
 
-def update_enemy_positions(enemy_list, score):
-    for idx, enemy_pos in enumerate(enemy_list):
-        if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
-            enemy_pos[1] += SPEED
+def update_enemy_positions(enemies, score):
+    for idx, enemy in enumerate(enemies):
+        if enemy.get_pos()[1] >= 0 and enemy.get_pos()[1] < HEIGHT:
+            enemy.get_pos()[1] += SPEED
         else:
             enemy_list.pop(idx)
             score += 1
     return score
 
 
-def collision_check(enemy_list, player_pos):
-    for enemy_pos in enemy_list:
-        if detect_collision(enemy_pos, player_pos):
+def collision_check(enemies, player):
+    for enemy_pos in enemies:
+        if detect_collision(enemy_pos, player):
             return True
     return False
 
 
-def detect_collision(player_pos, enemy_pos):
-    p_x = player_pos[0]
-    p_y = player_pos[1]
+def detect_collision(player, enemy):
+    p_x = player.get_pos()[0]
+    p_y = player.get_pos()[1]
 
-    e_x = enemy_pos[0]
-    e_y = enemy_pos[1]
+    e_x = enemy.get_pos()[0]
+    e_y = enemy.get_pos()[1]
 
-    if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x + enemy_size)):
-        if (e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y + enemy_size)):
+    if (e_x >= p_x and e_x < (p_x + player.get_size())) or (p_x >= e_x and p_x < (e_x + enemy.get_size())):
+        if (e_y >= p_y and e_y < (p_y + player.get_size())) or (p_y >= e_y and p_y < (e_y + enemy.get_size())):
             return True
     return False
 
@@ -99,33 +145,31 @@ while not game_over:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            x = player_pos[0]
-            y = player_pos[1]
+            x = p1.get_pos()[0]
+            y = p1.get_pos()[1]
 
             if event.key == pygame.K_LEFT:
-                x -= player_size
+                x -= p1.get_size()
             elif event.key == pygame.K_RIGHT:
-                x += player_size
+                x += p1.get_size()
 
-            player_pos = [x, y]
+            p1.pos = [x, y]
 
     screen.fill(BACKGROUND_COLOUR)
 
-    if detect_collision(player_pos, enemy_pos):
-        game_over = True
-
     drop_enemies(enemy_list)
-    score = update_enemy_positions(enemy_list, score)
-    SPEED = set_level(score, SPEED)
+    _score = update_enemy_positions(enemy_list, _score)
+    SPEED = set_level(_score, SPEED)
 
-    if collision_check(enemy_list, player_pos):
+    if collision_check(enemy_list, p1):
         game_over = True
-    text = "Score:" + str(score)
+
+    text = "Score:" + str(_score)
     label = myFont.render(text, 1, YELLOW)
     screen.blit(label, (WIDTH-210, HEIGHT-40))
 
     draw_enemies(enemy_list)
-    pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+    pygame.draw.rect(screen, RED, (p1.get_pos()[0], p1.get_pos()[1], p1.get_size(), p1.get_size()))
 
     clock.tick(30)
 
